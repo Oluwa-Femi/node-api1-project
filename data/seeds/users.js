cd2aead100d9c308d763e9af71f3f4637807daec
Oluwa-Femi <oluwatolablessing@gmail.com>
2019-10-31 14:27:06 +0000

    Refactored into a single file

--- data/seeds/users.js
+++ data/seeds/users.js
@@ -11,6 +11,18 @@ exports.seed = function(knex) {
           name: 'Frodo Baggins',
           bio: 'The ring bearer',
         },
+        {
+          name: 'Donald Trump',
+          bio: 'The 45th POTUS',
+        },
+        {
+          name: 'Barack Obama',
+          bio: 'The 44th POTUS',
+        },
+        {
+          name: 'George W. Bush',
+          bio: 'The 43rd POTUS',
+        },
       ]);
     });
 };
--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 // implement your API here
 const express = require("express");
+const {
+  getUserById,
+  getUsers,
+  newUser,
+  updateUser,
+  deleteUser
+} = require("./data/users");
 
 const cors = require("cors");
 const database = require("./data/db");
@@ -8,92 +15,14 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-function newUser(req, res) {
-  const name = req.body;
-  database
-    .insert(name)
-    .then(user => {
-        res.status(200).json({
-            success: true,
-            user
-          });
-          console.log(user);
-    })
-    .catch(err =>
-      res.status(500).json({
-        success: false,
-        err
-      })
-    );
-}
-
-function getUsers(req, res) {
-    database
-      .find()
-      .then(users => {
-        res.status(200).json({
-          success: true,
-          users
-        });
-      })
-      .catch(err =>
-        res.status(500).json({
-          success: false,
-          err
-        })
-      );
-  }
-  
-  function getUserById(req, res) {
-    const id = req.params.id;
-    database
-      .findById(id)
-      .then(user => {
-        res.status(200).json({
-          success: true,
-          user
-        });
-      })
-      .catch(err =>
-        res.status(500).json({
-          success: false,
-          err
-        })
-      );
-  }
-
-  function deleteUser(req, res) {
-    const id = req.params.id;
-    database
-      .remove(id)
-      .then(user => {
-        if (user) {
-          res.status(204).json({
-            success: true,
-            user
-          });
-        } else {
-          res.status(404).json({
-            success: false,
-            message: "User not found"
-          });
-        }
-      })
-      .catch(err => {
-        res.status(500).json({
-          success: false,
-          err
-        });
-      });
-  }
-
   app.post("/api/users", newUser);
   app.get("/api/users", getUsers);
   app.get("/api/users/:id", getUserById);
   app.delete("/api/users/:id", deleteUser);
+  app.put("/api/users/:id", updateUser);
 
 app.get("*", (req, res) => {
   res.send("Welcome");
 });
 
-app.listen(5000, () => console.log("API running on port 5000"));
+app.listen(8000, () => console.log("API running on port 8000"));
